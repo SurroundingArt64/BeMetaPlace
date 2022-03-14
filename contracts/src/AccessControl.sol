aa3cf2cec7ccdd84f0c9510244fe083a3ece6ca3
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-03-14 15:40:53 +0530

    feat: update nft contract

---
+++ contracts/src/AccessControl.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import './BaseRelayRecipient.sol';
+
+contract AccessControl is OwnableUpgradeable, BaseRelayRecipient {
+	mapping(address => bool) private _admins;
+	event AdminAccessSet(address admin, bool enabled);
+
+	function setAdminAccess(address admin, bool enabled) public onlyOwner {
+		_admins[admin] = enabled;
+		emit AdminAccessSet(admin, enabled);
+	}
+
+	function isAdmin(address admin) public view returns (bool) {
+		return _admins[admin] || admin == owner();
+	}
+
+	modifier onlyAdmin() {
+		require(isAdmin(_msgSender()), 'Caller is not admin');
+		_;
+	}
+
+	/**
+	 * return the sender of this call.
+	 * if the call came through our trusted forwarder, return the original sender.
+	 * otherwise, return `msg.sender`.
+	 * should be used in the contract anywhere instead of msg.sender
+	 */
+	function _msgSender()
+		internal
+		view
+		virtual
+		override(BaseRelayRecipient, ContextUpgradeable)
+		returns (address ret)
+	{
+		return BaseRelayRecipient._msgSender();
+	}
+}
--- contracts/src/BeMetaPlace.sol
+++ contracts/src/BeMetaPlace.sol
@@ -1,7 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 import '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol';
 
-contract BeMetaPlace is ERC721Upgradeable {
-	function initialize() public initializer {}
+import './AccessControl.sol';
+
+contract BeMetaPlace is ERC721URIStorageUpgradeable, AccessControl {
+	using CountersUpgradeable for CountersUpgradeable.Counter;
+
+	CountersUpgradeable.Counter private _tokenCounters;
+
+	function initialize(string memory name_, string memory symbol_)
+		public
+		initializer
+	{
+		__ERC721URIStorage_init();
+		__ERC721_init(name_, symbol_);
+		__Ownable_init();
+	}
+
+	function mint(address to_, string memory tokenURI_) public onlyAdmin {
+		_tokenCounters.increment();
+		uint256 _tokenId = _tokenCounters.current();
+
+		_mint(to_, _tokenId);
+
+		_setTokenURI(_tokenId, tokenURI_);
+	}
+
+	function setTokenURI(uint256 tokenId, string memory _tokenURI)
+		public
+		onlyAdmin
+	{
+		_setTokenURI(tokenId, _tokenURI);
+	}
+
+	function _msgSender()
+		internal
+		view
+		virtual
+		override(AccessControl, ContextUpgradeable)
+		returns (address ret)
+	{
+		return BaseRelayRecipient._msgSender();
+	}
 }
