0326ebad1e0b0073324f3b4d93a808a13c6accb8
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-03-14 15:55:05 +0530

    feat: bemetaplace.test.ts

--- contracts/test/fixture.ts
+++ contracts/test/fixture.ts
@@ -1,7 +1,23 @@
-import { deployments, ethers } from 'hardhat'
+import {
+	deployments,
+	ethers,
+	getNamedAccounts,
+	getUnnamedAccounts,
+} from 'hardhat'
+import { BeMetaPlace } from '../src/types'
+import { setupUser, setupUsers } from './utils'
 
 export const setupTest = deployments.createFixture(async () => {
 	await deployments.fixture(['BeMetaPlace'])
 
-	await ethers.getContract('BeMetaPlace')
+	const BeMetaPlace = (await ethers.getContract('BeMetaPlace')) as BeMetaPlace
+
+	const contracts = { BeMetaPlace }
+	const { deployer: deployerAddress } = await getNamedAccounts()
+	const userAddresses = await getUnnamedAccounts()
+
+	const deployer = await setupUser(deployerAddress, contracts)
+	const users = await setupUsers(userAddresses, contracts)
+
+	return { deployer, users, ...contracts }
 })
