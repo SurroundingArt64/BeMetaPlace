8fdfdd3db03cf7564fb122d0d849672cc654f02c
AquaRules <aquarules22@gmail.com>
2022-03-22 16:35:37 +0530

    feat(app): nav styles

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,13 +1,54 @@
 import React from 'react'
 import classes from './Navbar.module.scss'
+import {
+  TextInput,
+  TextInputProps,
+  ActionIcon,
+  useMantineTheme,
+  Burger,
+} from '@mantine/core'
+import { Search, ArrowRight, ArrowLeft } from 'tabler-icons-react'
+import { useBooleanToggle } from '@mantine/hooks'
 
-const Navbar = () => {
+const Navbar = (props: TextInputProps) => {
+  const theme = useMantineTheme()
+  const [opened, toggleOpened] = useBooleanToggle(false)
   return (
     <div className={classes.root}>
       <div className={classes.container}>
         <div className={classes.logo}>
           be<span>meta</span>place
         </div>
+        <TextInput
+          icon={<Search size={18} />}
+          radius='xl'
+          size='sm'
+          rightSection={
+            <ActionIcon size={24} radius='xl' color='yellow' variant='filled'>
+              {theme.dir === 'ltr' ? (
+                <ArrowRight size={12} />
+              ) : (
+                <ArrowLeft size={12} />
+              )}
+            </ActionIcon>
+          }
+          placeholder='Search...'
+          rightSectionWidth={38}
+          className={classes.search}
+          {...props}
+        />
+        <ul>
+          <li>Marketplace</li>
+          <li>Wallet</li>
+          <li>Contact</li>
+        </ul>
+        <Burger
+          opened={opened}
+          onClick={() => toggleOpened()}
+          size='sm'
+          color='white'
+          className={classes.burger}
+        />
       </div>
     </div>
   )
