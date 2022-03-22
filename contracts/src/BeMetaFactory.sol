6ae2c5db8b7e092aad6a4c87d30a1d88825ac51a
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-03-22 22:07:20 +0530

    feat: yarn lock

--- contracts/src/BeMetaFactory.sol
+++ contracts/src/BeMetaFactory.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
+import './AccessControl.sol';
+
+contract BeMetaFactory is AccessControl {
+	function initialize() external initializer {
+		__Ownable_init();
+	}
+}
