6654ee8817af6c56db2e249fd8921f8e4838c1cf
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-03-22 22:01:29 +0530

    feat: Secondary Sale

---
+++ contracts/deploy/2_secondary.ts
@@ -0,0 +1,29 @@
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+export const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+	const { deployments, getNamedAccounts } = hre
+	const { deploy } = deployments
+
+	const { deployer } = await getNamedAccounts()
+
+	await deploy('SecondarySale', {
+		from: deployer,
+		log: true,
+		skipIfAlreadyDeployed: true,
+		args: [],
+		proxy: {
+			execute: {
+				init: {
+					methodName: 'initialize',
+					args: [],
+				},
+			},
+			proxyContract: 'OptimizedTransparentProxy',
+			upgradeIndex: 0,
+		},
+	})
+}
+export default func
+func.tags = ['SecondarySale']
+func.dependencies = ['BeMetaPlace']
--- contracts/test/fixture.ts
+++ contracts/test/fixture.ts
@@ -4,17 +4,30 @@ import {
 	getNamedAccounts,
 	getUnnamedAccounts,
 } from 'hardhat'
-import { BeMetaPlace, BeMetaToken, PrimarySale } from '../src/types'
+import {
+	BeMetaPlace,
+	BeMetaToken,
+	PrimarySale,
+	SecondarySale,
+} from '../src/types'
 import { setupUser, setupUsers } from './utils'
 
 export const setupTest = deployments.createFixture(async () => {
-	await deployments.fixture(['BeMetaPlace', 'BeMetaToken', 'PrimarySale'])
+	await deployments.fixture([
+		'BeMetaPlace',
+		'BeMetaToken',
+		'PrimarySale',
+		'SecondarySale',
+	])
 
 	const BeMetaPlace = (await ethers.getContract('BeMetaPlace')) as BeMetaPlace
 	const PrimarySale = (await ethers.getContract('PrimarySale')) as PrimarySale
+	const SecondarySale = (await ethers.getContract(
+		'SecondarySale'
+	)) as SecondarySale
 	const BeMetaToken = (await ethers.getContract('BeMetaToken')) as BeMetaToken
 
-	const contracts = { BeMetaPlace, PrimarySale, BeMetaToken }
+	const contracts = { BeMetaPlace, PrimarySale, BeMetaToken, SecondarySale }
 	const { deployer: deployerAddress } = await getNamedAccounts()
 	const userAddresses = await getUnnamedAccounts()
 
