59232ddc035d23e2a34bb619b171821ec9be4641
AquaRules <aquarules22@gmail.com>
2022-03-25 00:49:53 +0530

    feat(tests): sec sale partial

--- contracts/test/BeMetaPlace.test.ts
+++ contracts/test/BeMetaPlace.test.ts
@@ -33,94 +33,150 @@ describe('BeMetaPlace.sol', () => {
 		expect(await BeMetaPlace.symbol()).to.equal('BMP')
 	})
 
-	it('cannot sale with un-allowed', async () => {
-		await expect(
-			deployer.PrimarySale.create('utr-1', BeMetaToken.address, 3600, 1)
-		).to.be.revertedWith('Not allowed token')
+	describe('Primary Sale', () => {
+		it('cannot sale with un-allowed', async () => {
+			await expect(
+				deployer.PrimarySale.create('utr-1', BeMetaToken.address, 3600, 1)
+			).to.be.revertedWith('Not allowed token')
+		})
+
+		it('can create sale with allowed token', async () => {
+			await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
+			await deployer.PrimarySale.create('link-1', BeMetaToken.address, 3600, 1)
+		})
+
+		it('create multiple buy [2]', async () => {
+			const amount = ethers.utils.parseEther('100')
+			await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
+			await deployer.PrimarySale.create(
+				'link-1',
+				BeMetaToken.address,
+				3600,
+				amount
+			)
+			await deployer.PrimarySale.create(
+				'link-2',
+				BeMetaToken.address,
+				3600,
+				amount
+			)
+			await deployer.PrimarySale.create(
+				'link-3',
+				BeMetaToken.address,
+				3600,
+				amount
+			)
+
+			await deployer.BeMetaToken.transfer(alice.address, amount)
+			await alice.BeMetaToken.approve(PrimarySale.address, amount)
+			await alice.PrimarySale.buy(2)
+
+			const values = (
+				await deployer.PrimarySale.getListings(deployer.address)
+			).map((elem: any) => elem.toNumber())
+			expect(values).to.deep.eq([1, 3])
+			const sales = (await deployer.PrimarySale.getSales(deployer.address)).map(
+				(sale: any) => sale.listingIndex.toNumber()
+			)
+			expect(sales).to.deep.eq([0, 1])
+			expect(await BeMetaPlace.ownerOf(2)).to.eq(alice.address)
+		})
+
+		it('can cancel sale', async () => {
+			const amount = ethers.utils.parseEther('100')
+			await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
+			await deployer.PrimarySale.create(
+				'cancel-1',
+				BeMetaToken.address,
+				3600,
+				amount
+			)
+			await deployer.PrimarySale.cancel(1)
+			const values = (
+				await deployer.PrimarySale.getListings(deployer.address)
+			).map((elem: any) => elem.toNumber())
+			expect(values).to.deep.eq([])
+		})
+
+		it('can cancel and re-list', async () => {
+			const amount = ethers.utils.parseEther('100')
+			await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
+			await deployer.PrimarySale.create(
+				'cancel-1',
+				BeMetaToken.address,
+				3600,
+				amount
+			)
+			await deployer.PrimarySale.cancel(1)
+			let values = (
+				await deployer.PrimarySale.getListings(deployer.address)
+			).map((elem: any) => elem.toNumber())
+			expect(values).to.deep.eq([])
+			await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
+			await deployer.PrimarySale.create(
+				'cancel-1',
+				BeMetaToken.address,
+				3600,
+				amount
+			)
+			values = (await deployer.PrimarySale.getListings(deployer.address)).map(
+				(elem: any) => elem.toNumber()
+			)
+			expect(values).to.deep.eq([2])
+		})
 	})
 
-	it('can create sale with allowed token', async () => {
-		await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
-		await deployer.PrimarySale.create('link-1', BeMetaToken.address, 3600, 1)
-	})
-
-	it('create multiple buy [2]', async () => {
-		const amount = ethers.utils.parseEther('100')
-		await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
-		await deployer.PrimarySale.create(
-			'link-1',
-			BeMetaToken.address,
-			3600,
-			amount
-		)
-		await deployer.PrimarySale.create(
-			'link-2',
-			BeMetaToken.address,
-			3600,
-			amount
-		)
-		await deployer.PrimarySale.create(
-			'link-3',
-			BeMetaToken.address,
-			3600,
-			amount
-		)
-
-		await deployer.BeMetaToken.transfer(alice.address, amount)
-		await alice.BeMetaToken.approve(PrimarySale.address, amount)
-		await alice.PrimarySale.buy(2)
-
-		const values = (
-			await deployer.PrimarySale.getListings(deployer.address)
-		).map((elem: any) => elem.toNumber())
-		expect(values).to.deep.eq([1, 3])
-		const sales = (await deployer.PrimarySale.getSales(deployer.address)).map(
-			(sale: any) => sale.listingIndex.toNumber()
-		)
-		expect(sales).to.deep.eq([0, 1])
-		expect(await BeMetaPlace.ownerOf(2)).to.eq(alice.address)
-	})
-
-	it('can cancel sale', async () => {
-		const amount = ethers.utils.parseEther('100')
-		await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
-		await deployer.PrimarySale.create(
-			'cancel-1',
-			BeMetaToken.address,
-			3600,
-			amount
-		)
-		await deployer.PrimarySale.cancel(1)
-		const values = (
-			await deployer.PrimarySale.getListings(deployer.address)
-		).map((elem: any) => elem.toNumber())
-		expect(values).to.deep.eq([])
-	})
-
-	it('can cancel and re-list', async () => {
-		const amount = ethers.utils.parseEther('100')
-		await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
-		await deployer.PrimarySale.create(
-			'cancel-1',
-			BeMetaToken.address,
-			3600,
-			amount
-		)
-		await deployer.PrimarySale.cancel(1)
-		let values = (await deployer.PrimarySale.getListings(deployer.address)).map(
-			(elem: any) => elem.toNumber()
-		)
-		expect(values).to.deep.eq([])
-		await deployer.PrimarySale.setAllowedERC20(BeMetaToken.address, true)
-		await deployer.PrimarySale.create(
-			'cancel-1',
-			BeMetaToken.address,
-			3600,
-			amount
-		)
-		values = (await deployer.PrimarySale.getListings(deployer.address)).map(
-			(elem: any) => elem.toNumber()
-		)
-		expect(values).to.deep.eq([2])
+	describe('Secondary Sale', () => {
+		it('cannot sale with un-allowed NFTAddress', async () => {
+			const amount = ethers.utils.parseEther('100')
+			await expect(
+				deployer.SecondarySale.create(
+					BeMetaPlace.address,
+					BeMetaToken.address,
+					'utr-1',
+					amount,
+					Date.now(),
+					Date.now() + 3600
+				)
+			).to.be.revertedWith('NFTAddress not allowed')
+		})
+		it('cannot sale with un-allowed CurrencyAddress', async () => {
+			const amount = ethers.utils.parseEther('100')
+			await deployer.SecondarySale.setAllowedNFTAddress(
+				BeMetaPlace.address,
+				true
+			)
+			await expect(
+				deployer.SecondarySale.create(
+					BeMetaPlace.address,
+					BeMetaToken.address,
+					'utr-1',
+					amount,
+					Date.now(),
+					Date.now() + 3600
+				)
+			).to.be.revertedWith('Currency not allowed')
+		})
+		it('Should be able to put on sale', async () => {
+			const amount = ethers.utils.parseEther('100')
+			await deployer.SecondarySale.setAllowedNFTAddress(
+				BeMetaPlace.address,
+				true
+			)
+			await deployer.SecondarySale.setAllowedCurrency(BeMetaToken.address, true)
+			await deployer.BeMetaPlace.setApprovalForAll(SecondarySale.address, true)
+			await deployer.SecondarySale.create(
+				BeMetaPlace.address,
+				BeMetaToken.address,
+				'utr-1',
+				amount,
+				Date.now(),
+				Date.now() + 3600
+			)
+			const values = (
+				await deployer.SecondarySale.getListings(deployer.address)
+			).map((elem: any) => elem.tokenId.toNumber())
+			expect(values).to.deep.eq([1])
+		})
 	})
 })
