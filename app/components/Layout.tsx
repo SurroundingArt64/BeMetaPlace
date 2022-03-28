29fc0a64cd12736fe34c86b3f07a809e9725ae42
AquaRules <aquarules22@gmail.com>
2022-03-28 19:37:41 +0530

    feat(app): fixed nav

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Navbar />
+      <div style={{ height: '80px' }} />
       {children}
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
