6b4ed35cfd00d7d6492bc3b61fbaff2e5c8ff83a
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-03-30 03:07:40 +0530

    feat: local

---
+++ contracts/deploy/0.75_nft_create.ts
@@ -0,0 +1,22 @@
+import { deployments, ethers } from 'hardhat'
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { BeMetaPlace, PrimarySale } from '../src/types'
+
+export const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+	const { getNamedAccounts } = hre
+
+	const { deployer } = await getNamedAccounts()
+	const BeMetaPlace = (await ethers.getContract(
+		'BeMetaPlace',
+		deployer
+	)) as BeMetaPlace
+
+	const tx = await BeMetaPlace.mint(deployer, 'ipfs://')
+	console.log('hash:', tx.hash)
+	const r = await tx.wait()
+	console.log('Receipt:', r.transactionHash)
+}
+export default func
+func.tags = ['NFTCreate']
+func.dependencies = []
