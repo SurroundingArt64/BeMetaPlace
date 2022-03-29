025c15a94b3875e74c8b0c6ef4c72fb9acc3db83
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-03-30 01:22:00 +0530

    fix: create

--- contracts/deploy/1.75_create.ts
+++ contracts/deploy/1.75_create.ts
@@ -17,7 +17,14 @@ export const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 		deployer
 	)) as PrimarySale
 
-	const tx = await BeMetaPlace.setAdminAccess(PrimarySale.address, true)
+	const tx = await PrimarySale.create(
+		'https://gateway.pinata.cloud/ipfs/QmWr11LeYF6GSUVZrWoKKHGZ6bZCwtz414PofyDR8sjDhL',
+		(
+			await deployments.get('BeMetaToken')
+		).address,
+		3600 * 30,
+		ethers.utils.parseEther('200')
+	)
 	console.log('hash:', tx.hash)
 	const r = await tx.wait()
 	console.log('Receipt:', r.transactionHash)
